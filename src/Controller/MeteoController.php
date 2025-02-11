e95dfd87d104b25edbcde2e81d6331f645f831dc
Quentin Avrain <quentin.avrain@gmail.com>
2025-02-11 16:34:38 +0100

    EG-13 Meteo : default route

--- src/Controller/MeteoController.php
+++ src/Controller/MeteoController.php
@@ -10,8 +10,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
-#[Route('/api/v1/test')]
+#[Route('/api/v1')]
 final class MeteoController extends AbstractController
 {
     public function __construct(
@@ -25,6 +26,21 @@ final class MeteoController extends AbstractController
     public function index(string $postal_code): Response
     {
         $response = $this->openMeteoApiClient->getMeteoData($postal_code);
+        return new JsonResponse($response, 200, ['Content-Type' => 'application/json']);
+    }
+
+    #[Route('/meteo', name: 'app_meteo_default')]
+    public function default(): Response
+    {
+        $user = $this->getUser();
+        if (!$user) {
+            throw new HttpException(401, 'Unauthorized');
+        }
+
+        $postal_code = $user->getPostalCode();
+
+        $response = $this->openMeteoApiClient->getMeteoData($postal_code);
+
         return new JsonResponse($response, 200, ['Content-Type' => 'application/json']);
     }
 }
